276b442b052c51e3fff26663a3f8450c2f8a1e1e
Mario Garcia <satriani@malmsteens-MacBook-Pro.local>
2013-04-26 15:10:45 +0200

    insertAfter now works as expected

--- src/main/java/challenge/CircularListImpl.java
+++ src/main/java/challenge/CircularListImpl.java
@@ -4,62 +4,138 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
+/**
+ * Quick implementation of a CircularList<E> interface. 
+ *
+ * One feature of this implementation is the use of Nullable<E> objects. 
+ * This idea's been taken from the Optional object in Scala. These type of objects help to avoid NPE
+ * because the method always return an object. That object can carry a value
+ * or not, but anyway you can always ask the object if it has a value or not.
+ *
+ * @author @marioggar
+ *
+ **/
 class CircularListImpl<E> implements CircularList<E>{
 
-	private E current = null;
-	private LinkedList<E> list = new LinkedList<E>();
-	private ListIterator<E> listIterator = null;
+	private Nullable<E> current = new Nullable<E>(null);
+	private LinkedList<Nullable<E>> list = new LinkedList<Nullable<E>>();
+	private ListIterator<Nullable<E>> listIterator;
 
-	public E next(){
-		if (current == null){
-			return null;
+	public CircularListImpl(){
+		listIterator = list.listIterator();
+	}
+
+	/**
+	 * This method returns the next element in the list and moves the header
+	 * to that element. So the current element once the method's been called
+	 * will be the next element.
+	 *
+	 * @return the element right after the current element
+	 *
+	 **/
+	public Nullable<E> next(){
+		if (current.isNull()){
+			return current;
 		}
-		if(current == list.getLast()){
+		if(list.getLast().equals(current)){
 			current = list.getFirst();
 			return current;
 		}
 		return listIterator.next();
 	}
 
-	public E previous(){				
-		if (current == null){
-			return null;
+	/**
+	 * This method returns the previous element in the list and moves the header
+	 * to that element. So the current element once the method's been called
+	 * will be the previous element.
+	 *
+	 * @return the element right before the current element
+	 *
+	 **/
+	public Nullable<E> previous(){				
+		if (current.isNull()){
+			return current;
 		}
-		if (current == list.getFirst()){
+		if (list.getFirst().equals(current)){
 			current = list.getLast();
 			return current;
 		}
 		return listIterator.previous();
 	}
 
-	public E current(){
+	/**
+	 * This method returns the first available element
+	 *
+	 * @return the first available element
+	 **/
+	public Nullable<E> current(){
 		return current;
 	}	
 
-	public E add(E added){
-		current = added;
+	/**
+	 * This method adds a new element to the list. The new element becomes the current
+	 * element
+	 *
+	 * @param added The element we want to add
+	 * @return a Nullable instance object carrying the inserted element
+	 *
+	 **/
+	public Nullable<E> add(E added){
+		current = new Nullable<E>(added);
 		list.add(current);
-		listIterator = list.listIterator(list.indexOf(current));
+		int index = list.indexOf(current);
+		listIterator = index <= 1 ? list.listIterator() : list.listIterator(index - 1);
 		return current;
 	}
 
-	public E insertAfter(E pointer,E added){
-		int index = list.indexOf(pointer);
-		list.set(index,added);
-		listIterator = list.listIterator(index);
-		current = added;
-
-		return added;
+	/**
+	 * This method inserts the second parameter right after where the first parameter is
+	 * within the list
+	 *
+	 * @param pointer The element we use to locate where the added object is going to be
+	 * @param added The element we want to add
+	 * @return the added element
+	 *
+	 **/
+	public Nullable<E> insertAfter(E pointer,E added){
+	 /* Wrapping current with Nullable */
+		current = new Nullable<E>(added);
+	 /* Building the pointer */
+		Nullable<E> pointerElement = new Nullable<E>(pointer);
+	 /* If we're at the end the it's a matter to add the new element to the list */
+		if (list.isEmpty() || list.getLast().equals(pointerElement)){
+			return add(added);
+		}
+	 /* Otherwise we have to get the next element after the pointer and shift the 
+	  * rest of elements to the right */
+		int index = list.indexOf(pointerElement) + 1; 
+		list.add(index,current);
+	 /* Setting the iterator */
+		listIterator = list.listIterator(list.indexOf(current));
+	 /* Returning current */
+		return current;
 	}
 
+	/**
+	 * This method helps to know whether the list is empty or not
+	 *
+	 * @return true if the list is empty false otherwise
+	 *
+	 **/
 	public boolean isEmpty(){
 		return list.isEmpty();
 	}
 
+	/**
+	 * This method removes all elements in the list
+	 **/
 	public void clear(){
 		list.clear();
 	}
 
+	/**
+	 * This method returns the number of elements this list has
+	 **/
 	public int size(){
 		return list.size();
 	}
--- src/test/groovy/challenge/CircularListSpecification.groovy
+++ src/test/groovy/challenge/CircularListSpecification.groovy
@@ -15,12 +15,33 @@ class CircularListSpecification extends Specification{
 			def current = list.current()
 			def next = list.next()
 			def previous = list.previous()
+		and: "Dealing with nulls"
+			list.add(null)
 		then: "We should get some values" 
-			current == "Malloc"
-			next == "John"
-			previous == "Malloc"
+			list.current().isNull()
+			next.value == "John"
+			previous.value == "Malloc"
 		and: "Size of the list"
-			list.size() == 3
+			list.size() == 4
+	}
+
+	def "Inserting after a given element"(){
+		setup: "Creating a given instance of a circular list"
+			def list = new CircularListImpl()
+		when: "Adding elements"
+			list.add("John")
+			list.add("Peter")
+			list.add("Malloc")
+			list.add("Andrew")
+			list.add("Troy")
+		and: "Inserting after a given element"
+			list.insertAfter("Malloc","Hugo")
+			println list.list
+		then: "Malloc should be found applying previous()"	
+			list.size() == 6
+		and: "The previous value should be"
+			list.current().getValue() == "Hugo"
+			list.previous().getValue() == "Malloc"
 	}
 
 
